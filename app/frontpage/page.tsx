46805b5463ec9ae10dc14ae4f673925708e20667
Anthony Nguyen <anthonybnguyen56@gmail.com>
2024-10-18 22:43:38 -0500

    Fixed eslint issues so can deploy front page

--- app/frontpage/page.tsx
+++ app/frontpage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef} from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -158,7 +158,7 @@ export default function Page() {
       clearInterval(rotateInterval); // Clear rotate interval
       window.removeEventListener("scroll", handleScroll); // Remove event listener
     };
-  }, []);
+  }, [flashcards.length]);
 
   const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId);
@@ -260,7 +260,7 @@ export default function Page() {
             <div className="container mx-auto flex flex-col lg:flex-row items-center">
               <div className="lg:w-1/2 lg:pr-16">
                 <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 leading-tight">
-                  Making learning history.
+                  Making learning simple.
                 </h2>
                 <p className="text-lg md:text-xl text-gray-600 mb-8 max-w-2xl">
                   FlashAI combines the power of artificial intelligence with
